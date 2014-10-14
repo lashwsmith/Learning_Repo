64373abe294f7dd6f695a0721623250d9f081701
Lash W Smith <lash.whi.smith@hp.com>
2014-10-14 14:33:57 +0000

    Initial novaboot script has been added

---
+++ novaboot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function read_func ()
+{
+echo "Booting New Server"
+
+echo "Please enter a Flavor ID from the list: "
+nova flavor-list
+read flavor_id
+
+echo "Please enter an Image ID: "
+glance image-list
+read image_id
+
+echo "Please enter a Keypair name: "
+nova keypair-list
+read key_pair
+
+echo "Please enter an Availability Zone: "
+nova availability-zone-list
+read avail_zone
+
+echo "Please enter a Network ID: "
+neutron net-list
+read net_id
+
+echo "Please enter a name for the instance: "
+read instance_name
+
+echo "Booting instance with the following information: "
+echo "Flavor: $flavor_id"
+echo "Image ID: $image_id"
+echo "Key Pair Name: $key_pair"
+echo "Availability Zone: $avail_zone"
+echo "Network ID: $net_id"
+echo "Instance Name: $instance_name"
+
+echo "Is the above information correct?"
+read user_input
+}
+
+function nova_boot ()
+{
+if [ $user_input != "y" ]; then
+        echo "Please re-enter your information"
+        read_func
+else
+        echo "Booting instance"
+        nova boot --flavor $flavor_id --image $image_id --key-name $key_pair --availability-zone $avail_zone --nic net-id=$net_id $instance_name
+fi
+}
+
+read_func
+
+nova_boot
