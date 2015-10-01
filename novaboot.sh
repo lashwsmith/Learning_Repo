bdac9c88a2d4ab132e8add69840604df71f1bb2d
Ubuntu <lash.whi.smith@hpe.com>
2015-10-01 10:20:15 +0000

    Added functionality to specify between ephemeral and persistent instances

--- novaboot.sh
+++ novaboot.sh
@@ -26,8 +26,8 @@ fi
 }
 
 
-#Function to store user input in variables for use in the nova command
-read_func ()
+#THIS FUNCTION WILL CREATE AN EPHEMERAL INSTANCE (NO BOOTABLE VOLUME)
+eph_func ()
 {
 echo "Booting New Server"
 
@@ -72,9 +72,58 @@ echo "Is the above information correct? (y/n): "
 read user_input
 }
 
+#THIS FUNCTION WILL USE A BOOTABLE VOLUME TO CREATE THE INSTANCE
+per_func ()
+{
+echo "Booting New Server"
 
-#Function to create the server based on user input.
-nova_confirm ()
+echo "Please enter the number of servers you would like to create: "
+read number
+
+echo "Please enter a Flavor ID from the list: "
+nova flavor-list
+read flavor_id
+
+#select the bootable volume to use
+echo "Please select the bootable volume ID"
+cinder list
+read volume_id
+
+echo "Please enter a Keypair name: "
+nova keypair-list
+read key_pair
+
+echo "Please enter an Availability Zone: "
+nova availability-zone-list
+read avail_zone
+
+echo "Please enter a Network ID: "
+neutron net-list
+read net_id
+
+echo "Please enter a name for the instance: "
+read instance_name
+
+echo "Would you like to make this instance a LAMP server? (y/n): "
+read lamp_check
+
+echo "Booting instance with the following information: "
+echo "Flavor: $flavor_id"
+echo "Image ID: $image_id"
+echo "Key Pair Name: $key_pair"
+echo "Availability Zone: $avail_zone"
+echo "Network ID: $net_id"
+echo "Instance Name: $instance_name"
+echo "LAMP stack: $lamp_check"
+
+echo "Is the above information correct? (y/n): "
+read user_input
+}
+
+
+
+#Function to create the EPHEMERAL instance based on user input.
+eph_confirm ()
 {
 while [ $user_input == "n" ]
 do
@@ -92,6 +141,27 @@ if [ $user_input == "y" ]; then
 	done
 fi
 }
+
+#Function to create the PERSISTENT instance based on user input.
+per_confirm ()
+{
+while [ $user_input == "n" ]
+do
+        echo "Please re-enter your information"
+        read_func
+        done
+
+if [ $user_input == "y" ]; then
+        i=1
+        while [[ $i -le $number ]]
+        do
+                echo "******Booting New Instance******"
+                nova boot --flavor $flavor_id --image $image_id --key-name $key_pair --availability-zone $avail_zone --block_device_mapping vda=$volume_id:::0 --nic net-id=$net_id $instance_name-$i
+        ((i = i + 1))
+        done
+fi
+}
+
 
 
 #Function to create the server with LAMP stack installed through user-data
@@ -191,13 +261,24 @@ echo "Please enter your Horizon Password"
 	OS_PASSWORD=$HOR_PASSWORD
 
 
+#instance type check (ephemeral or persistent)
+echo "Will this be an ephemeral or persistent instance?"
+read instance_type
+
+        if [ $instance_type == "ephemeral" ]; then
+		eph_func
+
+	elif [ $instance_type == "persistent" ]; then
+		per_func
+	
+	fi
+
 region_func
-read_func
 
 
 if [ $lamp_check == "n" ]; then
 
-	nova_confirm
+	eph_confirm
 
 elif [ $lamp_check == "y" ]; then
 
